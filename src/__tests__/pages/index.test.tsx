c20a54c0f381a3255d63f801c2ebbb36332ff76c
Anish <anish.maharjan@zendesk.com>
2023-07-30 14:02:43 +1000

    refactor(test): reorganize the test folder structure

--- src/__tests__/pages/index.test.tsx
+++ src/__tests__/pages/index.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react'
-import Home from '../pages/index'
+import Home from '@/pages/index'
 import '@testing-library/jest-dom'
 
 describe('Home', () => {
