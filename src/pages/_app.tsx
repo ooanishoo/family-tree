c9c7f16996cabc2ca0278c72f99de43de181f15e
Anish <anish.maharjan@zendesk.com>
2023-08-02 11:46:31 +1000

    feat(FamilyTree): display the family tree component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from '@zendeskgarden/react-theming'
+import { FamilyTreeProvider } from '@/components/FamilyTreeProvider'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+      <FamilyTreeProvider>
+        <Component {...pageProps} />
+      </FamilyTreeProvider>
+    </ThemeProvider>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,52 @@
-import { Inter } from 'next/font/google'
+import Drawer from '@/components/Drawer'
+import FamilyTree from '@/components/FamilyTree'
+import { useFamilyTree } from '@/components/FamilyTreeProvider'
+import SearchMember from '@/components/SearchMember'
+import SearchRelationship from '@/components/SearchRelationship'
+import { Button } from '@zendeskgarden/react-buttons'
+import { Poppins } from 'next/font/google'
+import { useState } from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({ subsets: ['latin'], weight: '300' })
 
 export default function Home() {
+  const { root } = useFamilyTree()
+  const [isOpen, setIsOpen] = useState(false)
+
+  const showDrawer = () => setIsOpen(true)
+  const closeDrawer = () => setIsOpen(false)
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`bg-white flex min-h-screen flex-col items-center justify-start p-2 ${poppins.className} text-center`}
     >
-      <h1 className="text-xl">Family Tree</h1>
+      <h1 className="text-4xl font-medium mb-10 text-slate-700">Family Tree</h1>
+      <div className="max-w-2xl mx-auto mb-8 ">
+        <h3 className="text-lg font-semibold text-gray-900 ">
+          Welcome to the Shan family tree
+        </h3>
+        <p className="my-4 text-gray-500">
+          Click on any one of the family members to add a child or a spouse.
+        </p>
+      </div>
+      <Button
+        isBasic
+        onClick={showDrawer}
+        title="Search family members"
+        aria-label="Search family members"
+        className="!mb-8"
+      >
+        Search family members
+      </Button>
+
+      <FamilyTree rootMember={root} />
+      <Drawer isOpen={isOpen} onClose={closeDrawer}>
+        <div className="flex flex-col gap-16">
+          <SearchRelationship />
+          <span className="w-full mr-4 h-[0.5px] bg-gray-300"></span>
+          <SearchMember />
+        </div>
+      </Drawer>
     </main>
   )
 }
